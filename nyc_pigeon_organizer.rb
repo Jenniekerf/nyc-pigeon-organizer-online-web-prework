b09ee738194dd2cffdd05162c1dcd3910cf9a268
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-25 18:58:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,13 @@
-def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, hash), result|
-    hash.each do |value, names|
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_data.each_with_object({}) do |(key, h), pigeon_list|
+    h.each do |value, names|
       names.each do |name|
-        result[name] ||= {}
-        result[name][key] ||= []
-        result[name][key] << value.to_s
-      end
-    end
+       pigeon_list[name] ||= {}
+       pigeon_list[name][key] ||= []
+       pigeon_list[name][key] << value.to_s
+     end
+   end
+ end
   end
-end
-
 
 
